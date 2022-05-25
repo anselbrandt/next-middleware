5e557d822d9590cd4023fdc336a68448131d049e
ansel brandt <ansel@anselbrandt.com>
2022-05-25 06:06:34 -0400

    Update middleware

--- web/pages/_middleware.ts
+++ web/pages/_middleware.ts
@@ -3,8 +3,9 @@ import type { NextFetchEvent, NextRequest } from "next/server";
 export default function middleware(req: NextRequest, event: NextFetchEvent) {
   const route = req.nextUrl.pathname;
   if (!route.includes("/api/metrics")) {
-    const timestamp = new Date();
-    const date = timestamp.toLocaleString();
+    const time = new Date();
+    const timestamp = time.getTime();
+    const date = time.toLocaleString();
     const referrer = req.referrer;
     const geo = req.geo?.country;
     const method = req.method;
